14b41e26c6c0a2f16b6b4b78ce6e11c4eb463862
Diego Neves <pdcassiano@gmail.com>
2020-06-08 16:44:53 -0300

    adicionado a função de percorer 1 nível de pastas

--- arquivos.js
+++ arquivos.js
@@ -1,15 +1,39 @@
 const fs = require('fs')
+
 const Arquivos = {
     listarArquivos(path) {
-        const listaDeArquivos = fs.readdirSync(path, (err, arquivos) => arquivos);
+        const lista = fs.readdirSync(path, (err, itens) => itens);
+        const listaDeArquivos = []
+        const listaDePastas = []
+
+        lista.forEach((item, index, array) => {
+            if(ePasta(item)) {
+                listaDePastas.push(item)
+            } else {
+                listaDeArquivos.push(item)
+            }
+        })
+
+        listaDePastas.forEach((item, index, array) => {
+            const listaTemp = fs.readdirSync(item, (err, itens) => itens);
+            listaTemp.forEach((item, index, array) => {
+                if(ePasta(item)) {
+                    //listaDePastas.push(item)
+                    console.log(`pasta: ${item}`)
+                } else {
+                    //listarArquivos.push(item)
+                    console.log(`arquivo: ${item}`)
+                }
+            })
+        })
         return listaDeArquivos.filter(this.filtrarVideos);
     },
-    
-    getNomeDoArquivo(arquivo) {
+       getNomeDoArquivo(arquivo) {
         const nome = arquivo.split('.');
         nome.pop();
         return nome.toString().replace(",",".");
     },
+    
     filtrarVideos (arquivo) { 
         const extensoes = [ 'mp4', 'mkv', 'flv', 'mov', 'avi', 'wmv', 'm4a', 'ts', 'webm' ]
         for (extensao of extensoes) {
@@ -19,4 +43,11 @@ const Arquivos = {
         }
     }
 };
+function ePasta (path) {
+    try {
+        return fs.lstatSync(path).isDirectory()
+    } catch {
+        return false
+    }       
+}
 module.exports = Arquivos;
---
+++ start.bat
@@ -0,0 +1,2 @@
+node converter.js
+pause
